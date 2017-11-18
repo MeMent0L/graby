dc569144694e9cbae141698e99fad4a4a2a2ff01
lrtrct <lrtrct@gmail.com>
2017-11-18 13:27:15 +0100

    Update graby.py
    Few changes:
    > Switched source of data to Coinmarketcap official API
    > Updated get method to 'requests' 
    > Saving file in comma-delimited-text file rather than table. No need for 'tabulate' library anymore.

--- graby.py
+++ graby.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python
-
 import sys
-import urllib2
+import requests
 import json
 import re
 from tabulate import tabulate
@@ -14,40 +12,45 @@ def get_position(json):
 
 count = 1200
 currency = "usd"
-url = "http://coinmarketcap.northpole.ro/api/v5/all.json"
-headers = ["Ticker","Name","Market Cap","Price","Available Supply","Volume 24h","Change 7h"]
+
+# Getting data directly from Coinmarketcap API as of 18/11/2017
+
+url = "https://api.coinmarketcap.com/v1/ticker/?limit=1200"
+
+headers = ["Ticker","Name","Market Cap [USD]","Price [USD]","Price [BTC]","Available Supply","Max Supply",\
+"Volume 24h [USD]","Change 1h", "Change 24h", "Change 7d"]
+
+API_tickers_list=['symbol','market_cap_usd','price_usd','price_btc','available_supply','max_supply',\
+'24h_volume_usd','percent_change_1h','percent_change_24h','percent_change_7d']
 
 if len(sys.argv) == 2:
         count = int(sys.argv[1])
 
-
-data = urllib2.urlopen(url).read()
-data = json.loads(data)['markets']
+data = requests.get(url).text
+data = json.loads(data)
 data.sort(key=get_position)
-btc_price = float(data[0]['price'][currency])
 
 table = []
 text_output = []
 
-for d in data[0:1]:
-    volume = float(d['volume24']['btc'])*btc_price
-    d_name=re.sub(r'.*\n', "", d['name']).strip() 
-    table.append([d['symbol'],d_name,d['marketCap'][currency],d['price'][currency],d['marketCap']['btc'],volume,d['change7h'][currency]])
+for d in data[0:count]:
 
-currency = "btc"
-for d in data[1:count]:
-        try:
-            volume = float(d['volume24']['btc'])*btc_price
-        except:
-            volume = 0
-        
-        d_name=re.sub(r'.*\n', "", d['name']).strip()   
-        table.append([d['symbol'],d_name,d['marketCap'][currency],d['price'][currency],d['marketCap']['btc'],volume,d['change7h'][currency]])
+        d_name=re.sub(r'.*\n', "", d['name']).strip()
+
+        for tickers in API_tickers_list:
+            if d[tickers] is None:
+                d[tickers] = 'n/a'
+
+        table.append([d['symbol'],d_name,d['market_cap_usd'],d['price_usd'],d['price_btc'],d['available_supply'],d['max_supply'],\
+        d['24h_volume_usd'],d['percent_change_1h'],d['percent_change_24h'],d['percent_change_7d']])
 
         #text_output.append(str(([d['symbol'],",",d['name'],",",d['marketCap'][currency],",",d['price'][currency],",",d['marketCap']['btc'],volume,",",d['change7h'][currency],"/n"]))
 
-print table
+output_table = ','.join(headers) + "\n"
 
-#f = open("ccdata.txt", "w")
-#f.write(tabulate(table,headers,numalign="left",stralign="left",floatfmt=".8f"))
+for lines in table:
+    output_table += ','.join(lines) +"\n"
 
+
+f = open("ccdata.txt", "w")
+f.write(output_table)
