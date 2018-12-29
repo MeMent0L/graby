fa759e47f25beadc5e601939d8c7ea678a0ec5a0
lrtrct <lrtrct@gmail.com>
2018-12-30 00:35:25 +0100

    Update graby.py

--- graby.py
+++ graby.py
@@ -1,53 +1,50 @@
+#!/usr/bin/python
+
 import sys
 import requests
 import json
+import pandas as pd
 
-def get_position(json):
-    try:
-        return int(json['position'])
-    except KeyError:
-        return 1000
+count = 2122
+currency = 'BTC'
 
-count = 1200
+###################################################################################
 
-# Getting data directly from Coinmarketcap API as of 18/11/2017
+### TO BE UPDATED WITH PERSONAL API KEY (line 25)
+### MORE INFO HERE: https://pro.coinmarketcap.com/signup 
 
-url = "https://api.coinmarketcap.com/v1/ticker/?limit=1200"
-
-headers = ["Ticker","Name","Market Cap [USD]","Price [USD]","Price [BTC]","Available Supply","Max Supply",\
-"Volume 24h [USD]","Change 1h", "Change 24h", "Change 7d"]
-
-API_tickers_list=['symbol','market_cap_usd','price_usd','price_btc','available_supply','max_supply',\
-'24h_volume_usd','percent_change_1h','percent_change_24h','percent_change_7d']
-
-if len(sys.argv) == 2:
-        count = int(sys.argv[1])
-
-data = requests.get(url).text
-data = json.loads(data)
-data.sort(key=get_position)
-
-table = []
-text_output = []
-
-for d in data[0:count]:
-
-        d_name=re.sub(r'.*\n', "", d['name']).strip()
-
-        for tickers in API_tickers_list:
-            if d[tickers] is None:
-                d[tickers] = 'n/a'
-
-        table.append([d['symbol'],d_name,d['market_cap_usd'],d['price_usd'],d['price_btc'],d['available_supply'],d['max_supply'],\
-        d['24h_volume_usd'],d['percent_change_1h'],d['percent_change_24h'],d['percent_change_7d']])
-
-        #text_output.append(str(([d['symbol'],",",d['name'],",",d['marketCap'][currency],",",d['price'][currency],",",d['marketCap']['btc'],volume,",",d['change7h'][currency],"/n"]))
-
-output_table = ','.join(headers) + "\n"
-
-for lines in table:
-    output_table += ','.join(lines) +"\n"
+###################################################################################
 
 
-f = open("ccdata.txt", "w")
-f.write(output_table)
+coins_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?sort=market_cap' \
+            + '&start=1&limit='+ str(count) + '&convert=' + str(currency)
+            # + '&start=1&limit='+ str(count) + '&cryptocurrency_type=tokens&convert=' + str(currency)
+
+btc_url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC&convert=USD'
+
+header={'X-CMC_PRO_API_KEY':'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'}
+
+coins_data = requests.get(coins_url,headers=header).json()
+btc_data = requests.get(btc_url,headers=header).json()
+
+df = pd.DataFrame([btc_data['data']['BTC']],columns=btc_data['data']['BTC'].keys())
+
+count = len(coins_data['data'])
+
+for i in range(count-1):
+    df = df.append(coins_data['data'][i+1],ignore_index=True )
+
+df2 = df.quote.apply(pd.Series)
+df3 = df2.USD.apply(pd.Series)
+df4 = df2.BTC.apply(pd.Series)
+df4 = df4.iloc[1:count]
+df_quote = pd.concat([df3.iloc[[0]], df4], ignore_index=True)
+
+df_platform = df.platform.apply(pd.Series)
+
+df_base = df[df.columns.difference(['quote', 'platform'])]
+
+frames = [df_base, df_quote, df_platform]
+processed_df = pd.concat(frames, axis=1)
+
+processed_df.to_csv('ccdata.csv')
